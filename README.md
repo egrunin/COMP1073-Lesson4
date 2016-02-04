6956075dbcd7fae176d4b8401a218808db04b25f
egrunin <rregtc@hotmail.com>
2016-02-04 10:57:57 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP1073-Lesson3
+
+COMP1073 - Client-Side Scripting - Lesson 3 @ Georgian College
---
+++ Scripts/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// setup your IIFE (immediatly invoked function expression)
+(function(){
+	
+	console.log("App Started..");
+	
+})();
