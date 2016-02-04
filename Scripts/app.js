fbeb6da3726ffb583cccf4628f1baadf36f8bf32
egrunin <rregtc@hotmail.com>
2016-02-04 11:50:57 -0500

    updated app.js with anonymous function

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,6 +7,7 @@
 	
 	
 	// declared a 'named function' way
+	/*
 	function replaceFirstParagraph() {
 		console.log("inside replaceFirstParagraph function");
 		var firstParagraph;
@@ -15,7 +16,19 @@
 
 		firstParagraph.innerHTML = "My New Paragraph Data";
 	}
+	*/
+	
+	// declare an anonymous function with named alias
+	var replaceFirstParagraph = function() {
+		console.log("inside replaceFirstParagraph function");
+		var firstParagraph;
 
+		firstParagraph = document.getElementById("firstParagraph")
+
+		firstParagraph.innerHTML = "My New Paragraph Data";
+	};
+	
+	
 	// call the replaceFirstParagraph function
 	replaceFirstParagraph();
 
